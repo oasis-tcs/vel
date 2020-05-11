247e9a9afc66c8540f322bbfc2de2322809089d4
Uwe Ryssel <uwe.ryssel@pure-systems.com>
2020-05-11 10:48:15 +0200

    Fix scripts to support directories with spaces

--- db/spec-0.8/validate/dtd.sh
+++ db/spec-0.8/validate/dtd.sh
@@ -1,4 +1,4 @@
 # DP0=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 DP0TEMP=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 DP0=$(cygpath -w "$DP0TEMP")
-java -jar $DP0/xjparse.jar -v -c $DP0/../catalog.xml $1
+java -jar "$DP0/xjparse.jar" -v -c "$DP0/../catalog.xml" "$1"
--- db/spec-0.8/validate/validate.sh
+++ db/spec-0.8/validate/validate.sh
@@ -9,14 +9,14 @@ echo "############################################################"
 echo Validating $1
 echo "############################################################"
 echo ============== Phase 1: DTD validation ==============
-sh $DP0/dtd.sh $1 2>&1 >output.txt
+sh "$DP0/dtd.sh" "$1" 2>&1 >output.txt
 errorReturn=$?
 if [ $errorReturn -eq 0 ]
 then echo No DTD validation errors.
 else cat output.txt; exit $errorReturn
 fi
 echo ============ Phase 2: Writing rules validation ============
-sh $DP0/xslt2.sh $1 $(cygpath -w $DP0/../oasis-spec-note.xsl) /dev/null 2>output.txt
+sh "$DP0/xslt2.sh" "$1" "$(cygpath -w "$DP0/../oasis-spec-note.xsl")" /dev/null 2>output.txt
 errorReturn=$?
 if [ $errorReturn -eq 0 ]
 then echo No writing rule validation errors.
